4af8f81803664b589c32040117dbe1533d18e634
Richard <102641165+richard-pyx@users.noreply.github.com>
2022-05-20 19:06:18 -0400

    ProjetoElevador-1.0

---
+++ ProjetoElevador/Models/Elevador.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Models
+{
+    public class Elevador
+    {
+
+        // Variáveis a serem utilizadas na programação.
+        int maxAndar = 0, maxQtdPessoas = 0, qtdPessoas = 0, andar = 0;
+
+
+        //Inicializamos o programa definindo a capacidade e quantidade de andares do elevador através dos dados fornecidos pelo usuário.
+        public void Inicializar()
+        {
+            //Pede ao usuário para indicar a capacidade total.
+            Console.Write("Digite o total de pessoas que cabem no elevador: ");
+
+            //Loop while para válidar o valor inserido.
+            do //Faz enquanto a condição do while não é atendida.
+            {
+                maxQtdPessoas = int.Parse(Console.ReadLine()); //Recebe o valor através do input do usuário e o guarda em uma variável.
+
+                //Verificando se o valor fornecido pelo usuário é válido.
+                if (maxQtdPessoas <= 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed; //Converte o texto para a cor vermelha, sinalizando algo errado.
+                    Console.WriteLine($"Quantidade de pessoas inválida = {maxQtdPessoas}, informe um novo valor:"); //Informa que o valor inserido é inválido.
+                    Console.ForegroundColor = ConsoleColor.Yellow; //Volta a cor padrão (Amarelo).
+                }
+            }
+            while (maxQtdPessoas <= 0); //Repete o loop enquanto a quantidade informada for menor ou igual a zero.
+
+            //Limpa o console.
+            Console.Clear();
+
+
+            //Repete-se o processo acima para determinar a quantidade de andares:
+            //Pede ao usuário para indicar a quantidade máxima de andares.
+            Console.Write("Digite a quantidade total de andares.\n" +
+                "*(lembre-se que o térreo é o andar '0')\n" +
+                "\n" +
+                "Quantidade total de andares: ");
+
+            //Loop while para válidar o valor inserido.
+            do //Faz enquanto a condição do while não é atendida.
+            {
+                maxAndar = int.Parse(Console.ReadLine()); //Armazena o valor fornecido pelo usuário.
+                
+                //Verificando se o valor indicado é válido.
+                if (maxAndar <= 0) {
+                    Console.ForegroundColor = ConsoleColor.DarkRed; //Converte o texto para a cor vermelha, sinalizando algo errado.
+                    Console.WriteLine($"O valor indicado = {maxAndar} é inválido, informe um novo valor:"); //Informa que o valor inserido é inválido.
+                    Console.ForegroundColor = ConsoleColor.Yellow; //Volta a cor padrão (Amarelo).
+                }
+            }
+            while (maxAndar <= 0); //Repete o loop enquanto a quantidade informada for menor ou igual a zero.
+        }
+        
+        //Cria-se o método de entrada, que utilizaremos para adicionar uma pessoa ao elevador.
+        public void Entrar()
+        {
+            if (qtdPessoas >= maxQtdPessoas) //Verifica se o elevador está lotado.
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Impossível, elevador lotado!!!"); //Informa que o elevador está lotado e que a ação não foi póssivel.
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                qtdPessoas += 1; //Caso haja espaço livre dentro do elevador, adiciona mais uma pessoa.
+            }
+
+            Console.WriteLine($"Pessoas dentro do elevador: {qtdPessoas}, capacidade máx: {maxQtdPessoas}"); //Informa a quantidade atual de pessoas no elevador.
+        }
+
+        //Cria-se um método 'sair', respónsavel por remover uma pessoa de dentro do elevador.
+        public void Sair()
+        {
+            if (qtdPessoas <= 0) //Verifica se ainda há pessoas para serem removidas do elevador.
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Impossível, elevador já está vazio!!!"); //Informa que o elevador está vazio.
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else //Caso ainda tenha, remove uma pessoa.
+            {
+                qtdPessoas -= 1;
+            }
+
+            Console.WriteLine($"Pessoas dentro do elevador: {qtdPessoas}, capacidade máx: {maxQtdPessoas}"); //Informa a quantidade atual de pessoas dentro do elevador.
+        }
+
+        //Método responsável por fazer o elevador subir um andar.
+        public void Subir()
+        {
+            if (andar >= maxAndar) //Verifica se o elevador já está no último andar (mais alto).
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("O elevador já está no último andar."); //Informa caso o elevador esteja no último andar (mais alto). 
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else //Caso não esteja no último, sobe mais um andar.
+            {
+                andar += 1;
+            }
+            Console.WriteLine($"Andar atual: {andar}"); //Informa o andar atual.
+        }
+
+        public void Descer()
+        {
+            if (andar <= 0) //Verifica se o elevador já está no térreo.
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("O elevador já está no térreo."); //Informa que o elevador está no térreo.
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else //Caso não esteja no térreo, ele desce um andar.
+            {
+                andar -= 1;
+            }
+            Console.WriteLine($"Andar atual: {andar}"); //Informa o andar atual.
+        }
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,80 @@
+﻿using ProjetoElevador.Models;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.DarkGray; //Define a cor do plano de fundo do terminal.
+            Console.ForegroundColor = ConsoleColor.Yellow; //Define a cor das letras.
+            Console.WriteLine("Bem-vindo(a) ao aplicativo de gerencimento de elevadores. Divirta-se!\n");
+
+            Elevador elevador = new Elevador(); //Inicia uma instância do objeto elevador.
+
+            elevador.Inicializar(); //Inicia o programa utilizando-se da função presente em 'Models/Elevador.cs'.
+
+            //Agora cria-se um menu para que o úsuario possa escolher dentre as opções.
+            //Inicializa-se uma variavel de controle do loop e das escolhas.
+            string opcaoEscolhida = "";
+            
+            Console.Clear(); //Limpa o console para exibir as opções de maneira clara.
+            Console.WriteLine("O elevador começa no térreo.");
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green; //Define a cor do texto para verde.
+                Console.WriteLine(@"Selecione o digito da opção que você deseja:
+                                        1 - Adiciona uma pessoa ao elevador.
+                                        2 - Remove uma pessoa do elevador.
+                                        3 - Sobe um andar.
+                                        4 - Desce um andar.
+                                        5 - Encerrar aplicação");
+                Console.ForegroundColor = ConsoleColor.Yellow; //Define a cor para DarkYellow (padrão).
+
+                //Variavel que guarda a opção escolhida.
+                opcaoEscolhida = Console.ReadLine();
+
+                Console.Clear(); //Limpa as opções anteriores.
+
+                //Utiliza-se um switch case para verificar e executar a opção escolhida.
+                switch (opcaoEscolhida)
+                {
+                    case "1": //Opção que adiciona uma pessoa ao elevador.
+                        {
+                            Console.WriteLine("Você escolheu adicionar uma pessoa ao elevador!");
+                            elevador.Entrar(); break;
+                        }
+                    case "2": //Opção que remove uma pessoa do elevador.
+                        {
+                            Console.WriteLine("Você escolheu remover uma pessoa de dentro do elevador.");
+                            elevador.Sair(); break;
+                        }
+                    case "3": //Opção que faz o elevador subir um andar.
+                        {
+                            Console.WriteLine("Subindo . . .");
+                            elevador.Subir(); break;
+                        }
+                    case "4": //Opção que faz o elevador descer um andar.
+                        {
+                            Console.WriteLine("Descendo . . .");
+                            elevador.Descer(); break;
+                        }
+                    case "5": //Encerra o programa.
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Encerrando o programa.");
+                            break;
+                        }
+                    default: //Caso a opção escolhida seja inválida.
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine($"Opção inválida: {opcaoEscolhida}, digite uma opção válida."); break;
+                        }
+                }
+            }
+            while (opcaoEscolhida != "5"); //Repete as ações acima enquanto o número escolhido for diferente da opção '5'.
+        }
+    }
+}
